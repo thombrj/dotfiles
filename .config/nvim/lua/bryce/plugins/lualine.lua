9f48f2c2cdbd59dca9e24c01d839e2d4fd4a2a47
Bryce Thompson <9068303+thombrj@users.noreply.github.com>
2025-07-15 16:10:35 +0800

    lualine file path relative

--- .config/nvim/lua/bryce/plugins/lualine.lua
+++ .config/nvim/lua/bryce/plugins/lualine.lua
@@ -1,7 +1,15 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('lualine').setup()
-    end
+  "nvim-lualine/lualine.nvim",
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup {
+      sections = {
+        lualine_c = { 
+          {
+            'filename', path = 1,
+          }
+        }
+      }
+    }
+  end
 }
