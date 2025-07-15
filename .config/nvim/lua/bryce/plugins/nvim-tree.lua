ccfc5dedb27a8628ea053d4c818f71adc7b26025
Bryce Thompson <9068303+thombrj@users.noreply.github.com>
2025-07-15 16:10:29 +0800

    nvim tree focus toggle

--- .config/nvim/lua/bryce/plugins/nvim-tree.lua
+++ .config/nvim/lua/bryce/plugins/nvim-tree.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  enabled = false,
   config = function()
     require("nvim-tree").setup({
       sort = {
@@ -11,20 +10,27 @@ return {
       },
       filters = {
         dotfiles = true
-      }
+      },
+      update_focused_file = {
+        enable = true
+      },
     })
+
     -- settings for nvim tree
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
     vim.opt.termguicolors = true
-    vim.keymap.set("n", "<leader><Space>", ":NvimTreeToggle<CR>")
-    vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<CR>")
-    vim.keymap.set('n', '<leader>ft', function()
-      if vim.fn.bufname():match 'NvimTree_' then
-        vim.cmd.wincmd 'p'
-      else
-        nvimtree.find_file(true)
-      end
-    end, { desc = 'nvim-tree: toggle' })
+
+    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+--    function toggle_nvimtree()
+--      if vim.fn.bufname():match 'NvimTree_' then
+--        vim.cmd.wincmd 'p'
+--      else
+--        vim.cmd('NvimTreeFindFile')
+--      end
+--    end
+--
+--    vim.keymap.set("n", "<leader><space>", ":lua toggle_nvimtree()<CR>", { desc = "Toggle nvim tree focus" })
   end
 }
