07e7f14a632993d914762802d5d75477bae225e4
Bryce Thompson <9068303+thombrj@users.noreply.github.com>
2025-08-06 09:45:53 +0800

    Add options from that minimalistic nvim video, hope im not overdoing it

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,145 @@
-require("bryce.core")
-require("bryce.lazy")
+-- Default theme and transparency (overridden by plugin config if set)
+vim.cmd.colorscheme("unokai")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
-local function change_dir_from_argv()
-  local argv = vim.fn.argv()
-  if #argv > 0 then
-    local path = argv[1]
-    local is_dir = vim.fn.isdirectory(path) ==1
+-- Basic settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
-    if is_dir then
-      vim.cmd('cd ' .. path)
+-- Indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+-- Search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+-- Visual settings
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "120"
+
+-- Behaviour settings
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir/")
+vim.opt.updatetime = 300
+vim.opt.timeoutlen = 500
+vim.opt.autoread = true
+vim.opt.autowrite = false
+vim.opt.clipboard = "unnamedplus"
+
+vim.diagnostic.config({
+  virtual_text = true
+})
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d"', { desc = "Delete without yanking" })
+
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
+
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split buffer vertically" })
+vim.keymap.set("n", "<leader>sh", ":hsplit<CR>", { desc = "Split buffer horizontally" })
+
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+vim.keymap.set("n", "<C-Right>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Navigate to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Navigate to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Navigate to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Navigate to right window" })
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down"})
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up"})
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up"})
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down"})
+
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+vim.keymap.set("n", "<leader>rc", ":e ~/.config/nivm/init.lua<CR>", { desc = "Edit nvim config" })
+
+vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { desc = "Open Lazy" })
+vim.keymap.set("n", "<A-S-f>", "<CMD>lua vim.lsp.buf.format()<CR>", { desc = "Format the current buffer with lsp" } )
+
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
     end
-  end
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimResized", {
+  group = augroup,
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup,
+  callback = function()
+    local dir = vim.fn.expand('<afile>:p:h')
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, 'p')
+    end
+  end,
+})
+
+vim.opt.wildmenu = true
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wildignore:append({ "*.o", "*.obj", "*.pyc", "*.class", "*.jar", "*.cs"})
+
+vim.opt.diffopt:append("linematch:60")
+
+vim.opt.redrawtime = 100000
+vim.opt.maxmempattern = 20000
+
+local undodir = vim.fn.expand("~/.vim/undodir")
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, "p")
 end
 
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = change_dir_from_argv
 })
+
+require('bryce')
