c85ef4ec081e4937bef33c50e8a7a1b5f7cec96e
Bryce Thompson <9068303+thombrj@users.noreply.github.com>
2025-10-06 08:18:56 +0800

    remove blink.cmp going back to nvim-cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,24 +1,7 @@
 return {
   {
-    "saghen/blink.cmp",
-    dependencies = { "rafamadriz/friendly-snippets" },
-    version = '1.*',
-    opts = {
-      keymap = {
-        preset = "default",
-        ["<Tab>"] = { 'select_next', 'snippet_forward', 'fallback' },
-        ["<S-Tab>"] = { 'select_prev', 'snippet_backward', 'fallback' },
-      },
-      appearance = {
-        nerd_font_variant = "mono"
-      },
-      completion = { documentation = { auto_show = false } },
-      sources = { default = { "lsp", "path", "snippets", "buffer" } },
-    }
-  },
-  {
     'hrsh7th/nvim-cmp',
-    enabled = false,
+    enabled = true,
     event = 'InsertEnter',
     dependencies = {
       {
