bdf2c79262ffa2d9c5f8520dc3008e93f4e53fa1
Bryce Thompson <9068303+thombrj@users.noreply.github.com>
2025-10-06 08:18:22 +0800

    Remove remember position autocmd for now

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -27,16 +27,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Move to last position on new file opened
-vim.api.nvim_create_autocmd("BufReadPost", {
-  group = augroup,
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufReadPost", {
+--   group = augroup,
+--   callback = function()
+--     local mark = vim.api.nvim_buf_get_mark(0, '"')
+--     local lcount = vim.api.nvim_buf_line_count(0)
+--     if mark[1] > 0 and mark[1] <= lcount then
+--       pcall(vim.api.nvim_win_set_cursor, 0, mark)
+--     end
+--   end,
+-- })
 
 -- Not sure?
 vim.api.nvim_create_autocmd("VimResized", {
