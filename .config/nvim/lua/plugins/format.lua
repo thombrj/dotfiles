c44568d5f0e00ce8f687a1ffdcdcfcf89739e7c3
Bryce Thompson <9068303+thombrj@users.noreply.github.com>
2025-10-21 14:07:57 +0800

    Remove this comment from format

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -32,11 +32,6 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
   },
 }
